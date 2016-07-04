1132cf787536ee5f60d7a8dc6c4bf4d690a885fc
若林慧佑 <wakabayashieisuke@wakabayunoMini2.quadsystem.local>
2016-07-04 17:02:08 +0900

    起動時、いきなり認証するのではなく認証無しでも使えるよう選択画面を表示する

--- app/src/main/java/jp/co/wakawaka/tumvie/MainActivity.java
+++ app/src/main/java/jp/co/wakawaka/tumvie/MainActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Token.existToken(this)) {
-            Intent intent = new Intent(this, CallbackActivity.class);
-            startActivity(intent);
-        } else {
-            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-            builder.enableUrlBarHiding();
-            CustomTabsIntent customTabsIntent = builder.build();
-            customTabsIntent.launchUrl(this, Uri.parse(BuildConfig.TUMBLR_OAUTH_URL));
-        }
-        finish();
+        findViewById(R.id.authentication).setOnClickListener(authenticationOnClickListener);
+        findViewById(R.id.rightnow).setOnClickListener(rightnowOnClickListener);
     }
+
+    View.OnClickListener authenticationOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (Token.existToken(MainActivity.this)) {
+                Intent intent = new Intent(MainActivity.this, CallbackActivity.class);
+                startActivity(intent);
+            } else {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.enableUrlBarHiding();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(MainActivity.this, Uri.parse(BuildConfig.TUMBLR_OAUTH_URL));
+            }
+            MainActivity.this.finish();
+        }
+    };
+
+    View.OnClickListener rightnowOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "coming soon!", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
