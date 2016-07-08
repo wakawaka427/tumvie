b0083ee6fa0570b8e4bfd35146a8754a5e006d4d
wakawaka <wakawaka.427@gmail.com>
2016-07-09 04:23:11 +0900

    タブにアイコン表示 検索機能実装

--- app/src/main/java/jp/co/wakawaka/tumvie/searchlist/SearchListFragment.java
+++ app/src/main/java/jp/co/wakawaka/tumvie/searchlist/SearchListFragment.java
@@ -1,11 +1,20 @@
 package jp.co.wakawaka.tumvie.searchlist;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.tumblr.jumblr.JumblrClient;
 import com.tumblr.jumblr.request.RequestBuilder;
@@ -30,7 +39,13 @@ import rx.schedulers.Schedulers;
 /**
  */
 public class SearchListFragment extends Fragment {
+    private static final int SEARCH_TARGET_BLOG = 0;
+    private static final int SEARCH_TARGET_TAG = 1;
+
     private JumblrClient jumblrClient;
+    private int searchTarget;
+    private String searchText;
+    private InputMethodManager inputMethodManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,20 +67,71 @@ public class SearchListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search_list, container, false);
 
+        inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        setSearchSpinner(view);
+        setSearchText(view);
+
         // リストビューを取得
         searchList = (ListView) view.findViewById(R.id.search_list);
         searchList.setOnScrollListener(new EndlessScrollListener() {
             @Override
             public boolean onLoadMore(int page, int totalItemsCount) {
+                if (offset == 0) {
+                    return false;
+                }
                 subscribeVideo();
                 return false;
             }
         });
-        adapter = new SearchListViewAdapter(searchList.getContext());
-        searchList.setAdapter(adapter);
-        subscribeVideo();
 
         return view;
+    }
+
+    private void setSearchSpinner(View view) {
+        // スピナー設定
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // ArrayAdapter を、string-array とデフォルトのレイアウトを引数にして生成
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                getContext(), R.array.search_targets, android.R.layout.simple_spinner_item);
+        // 選択肢が表示された時に使用するレイアウトを指定
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // スピナーにアダプターを設定
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Spinner spinner = (Spinner) adapterView;
+                searchTarget = (int) spinner.getSelectedItemId();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
+    private void setSearchText(final View view) {
+        EditText textView = (EditText) view.findViewById(R.id.editText);
+        textView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH ||
+                        i == EditorInfo.IME_ACTION_DONE ||
+                        keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
+                                keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    if (!keyEvent.isShiftPressed()) {
+                        offset = 0;
+                        searchText = String.valueOf(textView.getText());
+                        adapter = new SearchListViewAdapter(searchList.getContext());
+                        searchList.setAdapter(adapter);
+                        subscribeVideo();
+                        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     private void subscribeVideo() {
@@ -91,12 +157,7 @@ public class SearchListFragment extends Fragment {
             new Observable.OnSubscribe<Item>() {
                 @Override
                 public void call(Subscriber<? super Item> subscriber) {
-                    RequestBuilder requestBuilder = new RequestBuilder(jumblrClient);
-                    Map<String, Object> options = new HashMap<>();
-                    options.put("api_key", BuildConfig.CONSUMER_KEY);
-                    options.put("type", CallbackActivity.POST_TYPE_VIDEO);
-                    options.put("offset", offset);
-                    List<Post> posts = requestBuilder.get("/blog/glitteradio/posts", options).getPosts();
+                    List<Post> posts = getPosts();
                     for (Post post : posts) {
                         offset++;
                         VideoPost videoPost = (VideoPost) post;
@@ -123,6 +184,20 @@ public class SearchListFragment extends Fragment {
                         }
                     }
                     subscriber.onCompleted();
+                }
+
+                private List<Post> getPosts() {
+                    RequestBuilder requestBuilder = new RequestBuilder(jumblrClient);
+                    Map<String, Object> options = new HashMap<>();
+                    options.put("api_key", BuildConfig.CONSUMER_KEY);
+                    options.put("type", CallbackActivity.POST_TYPE_VIDEO);
+                    options.put("offset", offset);
+                    if (SEARCH_TARGET_BLOG == searchTarget) {
+                        return requestBuilder.get("/blog/" + searchText + "/posts", options).getPosts();
+                    } else {
+                        options.put("tag", searchText);
+                        return requestBuilder.get("/tagged", options).getPosts();
+                    }
                 }
             }
     );
